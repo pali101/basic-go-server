bbc7eafe4bdad2fa3951ff2f742c0261f6694298
Aashish Paliwal <aashish.paliwal@live.com>
2022-07-01 16:54:12 +0530

    Added basic template

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main(){
-	
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Server started at port 8080.")
+	if err := http.ListenAndServe(":8080",nil); err != nil {
+		log.Fatal(err)
+	}
 }
